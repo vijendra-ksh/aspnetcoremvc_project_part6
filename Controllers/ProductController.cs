e48505f85ab0c25a0b27bd25a67df46bd7a578fa
vijendra kushwaha <vijendra.ksh@gmail.com>
2020-02-08 20:44:17 +0530

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShop.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Controllers
+{
+public class ProductController : Controller
+{
+private readonly IProductRepository _productRepository;
+private readonly ICategoryRepository _categoryRepository;
+public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+{
+    _productRepository = productRepository;
+    _categoryRepository = categoryRepository;
+}
+public ViewResult List()
+{
+    return View(_productRepository.GetAllProduct);
+}
+
+}
+}
